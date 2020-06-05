a42b004d2a6420e313eb7adf67f41cde1a1e3c07
RitaHartnell <mel.soriano6123@gmail.com>
2020-06-05 00:05:54 -0400

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,31 @@
 class Customer
-  
+    @@all = []
+
+    def initialize(name, age)
+        @name = name
+        @age = age
+        @@all << self
+    end
+
+    def new_meal(waiter, total, tip)
+        meal = Meal.new(waiter, self, total, tip)
+    end
+
+    def meals
+        Meal.all.select { |meal|
+            meal.customer == self
+        }
+    end
+
+    def waiters
+        my_meals = self.meals
+        my_meals.map { |meal|
+            meal.waiter
+        }
+    end
+    
+    def self.all
+        @@all
+    end
+
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,37 @@
 class Waiter
+    @@all = []
+
+    def initialize(name, xp)
+        @name = name
+        @experience = xp
+        @@all << self
+    end
+
+    def new_meal(customer, total, tip)
+        meal = Meal.new(self, customer, total, tip)
+    end
+
+    def meals
+        Meal.all.select { |meal|
+            meal.waiter == self
+        }
+    end
+
+    def best_tipper
+        my_meals= self.meals
+        tips = my_meals.map { |meal|
+            meal.tip
+        }
+
+        best = my_meals.select { |meal|
+            meal.tip == tips.max
+        }
+        best[0].customer
+        
+    end
+
+    def self.all
+        @@all
+    end
 
 end
